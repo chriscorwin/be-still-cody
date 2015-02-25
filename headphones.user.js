c98248eb46ba04b01cf25d06bfee840531e10bca
Chris Corwin <CCorwin@exacttarget.com>
2015-02-25 10:50:38 -0500

    added a git ignore in prep of adding some gulp or grunt things. Formatted the javascript, whitespace changes only, in prep for making modificaitons and additions easier to merge.

--- headphones.user.js
+++ headphones.user.js
@@ -11,10 +11,10 @@
 // ==/UserScript==
 
 //make tabs easier to access by assigning their text as class names
-$('#tabBar li').each(function(i, el){
-	var $el = $(el);
-	$el.addClass($el.text().toLowerCase());
-});
+$( '#tabBar li' ).each( function( i, el ) {
+	var $el = $( el );
+	$el.addClass( $el.text().toLowerCase() );
+} );
 
 // Trash BS
 [
@@ -39,58 +39,58 @@ $('#tabBar li').each(function(i, el){
 	'#tabBar .partner.applications',
 	'#tabBar .sales.central',
 	'#tabBar .companies',
-	'.recElement.todoElement',//trash annoyingly bright yellow box thing
-	'#section_header',//trash the amazing-space-eating "hide/show feed" button that for some reason needs its own huge bar. Well, trash the whole bar!
-	'#ptBody',//trash pointless redundant egotistical feed header
-	'.zen-branding',//trash logo
-	'a[href^="javascript:openPopupFocusEscapePounds"]',//remove useless links to help docs
-	'.chatterUserStatus',//trash redundant profile pic and link in Chatter
-	'.headerContent'//trash pointless huge "salesforce.com" text and invisible header bar
-].forEach(function(el, i, bs){
-	$(el).remove();
-});
+	'.recElement.todoElement', //trash annoyingly bright yellow box thing
+	'#section_header', //trash the amazing-space-eating "hide/show feed" button that for some reason needs its own huge bar. Well, trash the whole bar!
+	'#ptBody', //trash pointless redundant egotistical feed header
+	'.zen-branding', //trash logo
+	'a[href^="javascript:openPopupFocusEscapePounds"]', //remove useless links to help docs
+	'.chatterUserStatus', //trash redundant profile pic and link in Chatter
+	'.headerContent' //trash pointless huge "salesforce.com" text and invisible header bar
+].forEach( function( el, i, bs ) {
+	$( el ).remove();
+} );
 
 //trash BS that loads really slowly
 var trashSlowBS = function() {
 	[
 		'#presence_widget'
-	].forEach(function(el, i, bs){
-		$(el).remove();
-	});
-	window.setTimeout(trashSlowBS, 2000);
+	].forEach( function( el, i, bs ) {
+		$( el ).remove();
+	} );
+	window.setTimeout( trashSlowBS, 2000 );
 }
 trashSlowBS();
 
 //hide hidden items, and add hide to remaining
-var hiddenChats = JSON.parse(localStorage.getItem('hiddenChats')) || []; 
-console.log('hiddenChats', hiddenChats);
+var hiddenChats = JSON.parse( localStorage.getItem( 'hiddenChats' ) ) || [];
+console.log( 'hiddenChats', hiddenChats );
 var enableHiddenChatsFeature = function enableHiddenChatsFeature() {
-    $('.feeditem').each(function(i, el){
-        var $el = $(el);
-        var id = $el[0].id;
-        var hiddenChatI = hiddenChats.indexOf(id);
+	$( '.feeditem' ).each( function( i, el ) {
+		var $el = $( el );
+		var id = $el[ 0 ].id;
+		var hiddenChatI = hiddenChats.indexOf( id );
 
-        if(hiddenChatI >= 0){
-            $el.remove();
-        }else{
-            if($el.find('.hideFeedItem').length <= 0){
-                $el.find('.preamblecontainer.displayblock').append('<a href="remove' + id + '" class="hideFeedItem" data-id="' + id + '">hide</a>');
-            }
-        }
-    });
+		if ( hiddenChatI >= 0 ) {
+			$el.remove();
+		} else {
+			if ( $el.find( '.hideFeedItem' ).length <= 0 ) {
+				$el.find( '.preamblecontainer.displayblock' ).append( '<a href="remove' + id + '" class="hideFeedItem" data-id="' + id + '">hide</a>' );
+			}
+		}
+	} );
 
-    $('.hideFeedItem').on('click', function(e){
-        e.preventDefault();
-        var $this = $(this);
-        hiddenChats.unshift($this.data('id'));
-        localStorage.setItem('hiddenChats', JSON.stringify(hiddenChats));
-        $this.closest('.feeditem').remove();
-    });
-    
-    window.setTimeout(enableHiddenChatsFeature, 4500);
+	$( '.hideFeedItem' ).on( 'click', function( e ) {
+		e.preventDefault();
+		var $this = $( this );
+		hiddenChats.unshift( $this.data( 'id' ) );
+		localStorage.setItem( 'hiddenChats', JSON.stringify( hiddenChats ) );
+		$this.closest( '.feeditem' ).remove();
+	} );
+
+	window.setTimeout( enableHiddenChatsFeature, 4500 );
 }
 enableHiddenChatsFeature();
 
-$('.cxshowmorefeeditemscontainer.showmorefeeditemscontainer a').on('click', function(e){
-    window.setTimeout(enableHiddenChatsFeature, 4500);
-});
+$( '.cxshowmorefeeditemscontainer.showmorefeeditemscontainer a' ).on( 'click', function( e ) {
+	window.setTimeout( enableHiddenChatsFeature, 4500 );
+} );
