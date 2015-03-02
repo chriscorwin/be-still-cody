9436976334481a0e09391576c6bc1bec2a37cde4
Christopher McCulloh <cmcculloh@gmail.com>
2015-03-02 14:07:21 -0500

    (master) updates version number

--- headphones.user.js
+++ headphones.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        Be Still, Cody
 // @namespace   http://chomperstomp.com
-// @version     0.1.0+030
+// @version     0.1.0+031
 // @description Cut out the useless Chatter
 // @author      Christopher McCulloh
 // @contributor Chris Corwin
