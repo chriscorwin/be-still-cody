1724582c044819b1b483b37f4f4295b0991314c4
Christopher McCulloh <cmcculloh@gmail.com>
2015-03-03 13:03:07 -0500

    (master) puts name first so that it is the last to get truncated it title is too long

--- headphones.user.js
+++ headphones.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        Be Still, Cody
 // @namespace   http://chomperstomp.com
-// @version     0.1.0+063
+// @version     0.1.0+064
 // @description Cut out the useless Chatter
 // @author      Christopher McCulloh
 // @contributor Chris Corwin
@@ -138,6 +138,9 @@ var betterFeedItemActions = function betterFeedItemActions() {
 		var id = $el[0].id;
 
 		var $feeditemTitle = $el.find('.preamblecontainer').detach();
+		var $person = $feeditemTitle.find('.feeditemsecondentity').detach();
+		var $group = $feeditemTitle.find('.feeditemfirstentity').detach();
+		$feeditemTitle.empty().append($person, $('<span> - </span>'), $group);
 		$el.find('.panel-heading').append($feeditemTitle);
 
 		var $topics = $el.find('.topics').detach();
